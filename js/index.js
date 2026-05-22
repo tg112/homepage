e14d992eaa403bbfafeceef1b3137441365d20b8
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-05-22 16:55:19 +0900

    feat: add slideshow

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const images = [
+    "images/bg.jpg",
+    "images/bg2.jpg",
+    "images/bg3.jpg",
+    "images/bg4.jpg",
+    "images/bg5.jpg",
+];
+
+const imageElement = document.querySelector(".img");
+
+let currentIndex = 0;
+
+imageElement.src = images[currentIndex];
+
+setInterval(() => {
+    currentIndex++;
+
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    imageElement.setAttribute("src", images[currentIndex]);
+}, 3000);
